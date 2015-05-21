72bff7d3670db01cf51a9e3e2feb947e740a3867
LeAlbus <jpedro_lopes21@hotmail.com>
2015-05-21 20:01:53 -0300

    ambiente preparado

--- SpriteKit_SE/SpriteKit_SE/GameScene.swift
+++ SpriteKit_SE/SpriteKit_SE/GameScene.swift
@@ -9,6 +9,20 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    required init?(coder aDecoder: NSCoder) {
+        
+        super.init(coder: aDecoder)
+    }
+    
+    override init(size: CGSize) {
+        
+        super.init(size: size)
+        
+        backgroundColor = SKColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
+    }
+
+    /*
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
         let myLabel = SKLabelNode(fontNamed:"Chalkduster")
@@ -41,5 +55,5 @@ class GameScene: SKScene {
    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
-    }
+    }*/
 }
